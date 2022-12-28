09a27ab85458d7b3d1448358b80e90e722d98a34
Oyugi-Kevin <mr.kevin.oyugi@gmail.com>
2022-12-28 08:23:26 -0800

    function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main: add - adds two integers
+ * Definition:
+ * @n1: integer 1
+ * @n2: integer 2
+ *
+ * Description: give two integers, returns sum
+ *
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
