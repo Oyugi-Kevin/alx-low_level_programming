a135b6d00a1154e4151b293be2332374cd5c0cc2
Oyugi-Kevin <mr.kevin.oyugi@gmail.com>
2023-01-25 11:11:54 -0800

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,4 +11,14 @@ int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
+int is_digit(char *s);
+
+int _strlen(char *s);
+
+void errors(void);
+
+int main(int argc, char *argv[]);
+
+int _putchar(char c);
+
 #endif
